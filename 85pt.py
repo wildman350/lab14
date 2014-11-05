e8fdc347eaa7e14cd678926583996874c21253d5
wildman350 <s581776@student.roundrockisd.org>
2014-11-05 15:35:53 -0600

    85 pt done

--- 85pt.py
+++ 85pt.py
@@ -33,12 +33,15 @@ class MyApp:
 		# "Bind" an action to the first button												
 		self.button1.bind("<Button-1>", self.button1Click)
 		 
-		  
+		self.button2 = Button(self.myContainer1)
+		self.button2.configure(text="right", background= "green")
+		self.button2.grid(row=0,column=1)
+		
+	
+		# "Bind" an action to the first button												
+		self.button2.bind("<Button-1>", self.button2Click) 
 		# This creates the drawpad - no need to change this 
 		drawpad.pack()
-		
-
-		
 	def button1Click(self, event):   
 		# Make the oval move to the left!
                 # "global" makes sure that we can access our oval and our drawpad
@@ -47,7 +50,22 @@ class MyApp:
 		global drawpad
 		global drawpadwidth
 		global drawpadheight
-	
+	        x1,y1,x2,y2 = drawpad.coords(oval)
+	        if x1 > 0:
+	            drawpad.move(oval,-20,0)
+
+		
+	def button2Click(self, event):   
+		# Make the oval move to the left!
+                # "global" makes sure that we can access our oval and our drawpad
+                # Add in boundary detection
+		global oval
+		global drawpad
+		global drawpadwidth
+		global drawpadheight
+	        x1,y1,x2,y2 = drawpad.coords(oval)
+	        if x2 < 475:
+	            drawpad.move(oval,20,0)
 	# Add the button2Click method
 		
 myapp = MyApp(root)
